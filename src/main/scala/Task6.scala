94a067878089656c52c561802c4366f3f390b25b
gaborloi <gaborloi@gmail.com>
2025-12-12 23:28:04 +0100

    Task 6.1 finish

--- src/main/scala/Task6.scala
+++ src/main/scala/Task6.scala
@@ -11,8 +11,35 @@ object Task6 extends Task {
         case "*" => arrayList.dropRight(1).map(_(i).toLong).product + sum
     }
   }
-  
-  def solveProblem2(file: BufferedSource): Int = {
-    2
+
+  def solveProblem2(file: BufferedSource): Long = {
+    val iterList = file.getLines().map(_.iterator).toList
+
+    parseNum(iterList, 0, 0, ' ')
+  }
+
+  @tailrec
+  def parseNum(iterList: List[Iterator[Char]], sumValue: Long, sumTotal: Long, transformation: Char): Long = {
+    val values = iterList.map { it => if(it.hasNext) it.next() else ' ' }
+    val currentStr = values.dropRight(1).foldLeft("") { (s, c) => s + c.toString }.strip
+    val currentNum = if (currentStr.isEmpty) 0L else currentStr.toLong
+
+    if(iterList.forall(!_.hasNext)) {
+      val newSumValue = transformation match
+        case '+' => sumValue + currentNum
+        case '*' => sumValue * currentNum
+      sumTotal + newSumValue
+    } else {
+      if (currentNum == 0L) parseNum(iterList, sumValue, sumTotal, transformation) else {
+        if (values.last.isWhitespace) {
+          val newSumValue = transformation match
+            case '+' => sumValue + currentNum
+            case '*' => sumValue * currentNum
+          parseNum(iterList, newSumValue, sumTotal, transformation)
+        } else {
+          parseNum(iterList, currentNum, sumTotal + sumValue, values.last)
+        }
+      }
+    }
   }
 }
--- src/test/scala/TestTask6Suite.scala
+++ src/test/scala/TestTask6Suite.scala
@@ -11,7 +11,7 @@ class TestTask6Suite extends AnyFunSuite {
     "task" + taskNr + "_2 test file",
     "task" + taskNr + "_2 all file"
   )
-  val outputs: List[AnyVal] = List(4277556L, 7644505810277L, 14L, 350684792662845L)
+  val outputs: List[AnyVal] = List(4277556L, 7644505810277L, 3263827L, 12841228084455L)
   test(tasknames.head) {
     assert(task.solveProblem1(Source.fromResource(s"input_test_${task.id}.txt")) == outputs.head)
   }
@@ -20,11 +20,11 @@ class TestTask6Suite extends AnyFunSuite {
     assert(task.solveProblem1(Source.fromResource(s"input_all_${task.id}.txt")) == outputs(1))
   }
 
-//  test(tasknames(2)) {
-//    assert(task.solveProblem2(Source.fromResource(s"input_test_${task.id}.txt")) == outputs(2))
-//  }
-//
-//  test(tasknames(3)) {
-//    assert(task.solveProblem2(Source.fromResource(s"input_all_${task.id}.txt")) == outputs(3))
-//  }
+  test(tasknames(2)) {
+    assert(task.solveProblem2(Source.fromResource(s"input_test_${task.id}.txt")) == outputs(2))
+  }
+
+  test(tasknames(3)) {
+    assert(task.solveProblem2(Source.fromResource(s"input_all_${task.id}.txt")) == outputs(3))
+  }
 }
