7634681246d25ac2d735cd73c6bf3e0590d88b69
gaborloi <gaborloi@gmail.com>
2025-12-12 23:37:43 +0100

    Task 7 prep

---
+++ src/main/scala/Task7.scala
@@ -0,0 +1,13 @@
+import scala.annotation.tailrec
+import scala.io.BufferedSource
+
+object Task7 extends Task {
+  val id = 7
+  def solveProblem1(file: BufferedSource): Int = {
+    1
+  }
+
+  def solveProblem2(file: BufferedSource): Long = {
+    2L
+  }
+}
