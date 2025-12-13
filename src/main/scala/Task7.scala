6ead69be6cf3bf034dd827caccc6e298f661e8bb
gaborloi <gaborloi@gmail.com>
2025-12-13 23:08:39 +0100

    Task 7.2

--- src/main/scala/Task7.scala
+++ src/main/scala/Task7.scala
@@ -6,37 +6,45 @@ object Task7 extends Task {
   val id = 7
   val PRISM = '^'
   val SCATTER: List[Cord] = List(Cord(0,-1), Cord(0,1))
-  case class Beam(cord: Cord) {
-    def fall(map: Array[Array[Char]], nextBeams: Set[Beam], activatedPrisms: Set[Cord]): (Set[Beam], Set[Cord]) = {
+  case class Beam(cord: Cord, weight: Long) {
+    def fall(map: Array[Array[Char]], nextBeams: List[Beam], activePrisms: Set[Cord]): (List[Beam], Set[Cord]) = {
       val newCord = cord + Cord(1, 0)
-      val (nextCord, activatedPrismsUpd) = checkCord(newCord, map, nextBeams.map(_.cord), activatedPrisms)
-      (nextCord.map(Beam.apply), activatedPrismsUpd)
+      checkCord(newCord, weight, map, nextBeams, activePrisms)
     }
-
-    final def checkCord(
-      cord: Cord,
-      map: Array[Array[Char]],
-      nextCords: Set[Cord],
-      activatedPrisms: Set[Cord]
-    ): (Set[Cord], Set[Cord]) =
-      if (map(cord.r)(cord.c) == PRISM) {
-        if (activatedPrisms.contains(cord)) (nextCords, activatedPrisms) else {
-          val activatedPrismsUpd = activatedPrisms + cord
-          SCATTER.map { dir =>
-            val sCord = cord + dir
-            if (sCord.valid(map.length, map(0).length))
-              checkCord(sCord, map, nextCords, activatedPrismsUpd) else (nextCords, activatedPrismsUpd)
-          }.reduce( (a,b) => (a._1.union(b._1), a._2.union(b._2)))
-        }
-      } else (nextCords + cord, activatedPrisms)
   }
+  final def checkCord(
+    cord: Cord,
+    weight: Long,
+    map: Array[Array[Char]],
+    nextBeams: List[Beam],
+    activePrisms: Set[Cord]
+  ): (List[Beam], Set[Cord]) =
+    if (map(cord.r)(cord.c) == PRISM) {
+      val activePrismsUpd = activePrisms + cord
+      val additions: List[Beam] = SCATTER.map { dir => Beam(cord + dir, weight) }
+      val consolidatedList: List[Beam] = List(nextBeams, additions).reduce { (a, b) =>
+        b.foldLeft(a) { (l, bb) =>
+          val iOps = l.indexWhere(lb => bb.cord == lb.cord)
+          iOps match
+            case -1 => l :+ bb
+            case i =>
+              val newWeight = bb.weight + l(i).weight
+              l.take(i) ++ l.drop(i + 1) :+ Beam(bb.cord, newWeight)
+        }
+      }
+      (consolidatedList, activePrismsUpd)
+    } else {
+//      println(s"${nextBeams :+ Beam(cord, weight)}")
+      (nextBeams :+ Beam(cord, weight), activePrisms)
+    }
 
   def solveProblem1(file: BufferedSource): Int = {
     val map = fileToCharArray(file)
     val start = Cord(0, map.head.indexOf('S'))
-    val startBeam = Beam(start)
-    val (finalBeams, activatedPrisms) = Range(0, map.length - 1).foldLeft((Set(startBeam), Set[Cord]())) {
-      case ((sB, sP), _) => sB.foldLeft((Set[Beam](), sP)) { case ((sB2, sP2), cB) =>
+    val startBeam = Beam(start, 1)
+    val (finalBeams, activatedPrisms) = Range(0, map.length - 1).foldLeft((List(startBeam), Set[Cord]())) {
+      case ((sB, sP), _) =>
+        sB.foldLeft((List[Beam](), sP)) { case ((sB2, sP2), cB) =>
         cB.fall(map, sB2, sP2)
       }
     }
@@ -44,6 +52,14 @@ object Task7 extends Task {
   }
 
   def solveProblem2(file: BufferedSource): Long = {
-    2L
+    val map = fileToCharArray(file)
+    val start = Cord(0, map.head.indexOf('S'))
+    val startBeam = Beam(start, 1)
+    val (finalBeams, activatedPrisms) = Range(0, map.length - 1).foldLeft((List(startBeam), Set[Cord]())) {
+      case ((sB, sP), _) => sB.foldLeft((List[Beam](), sP)) { case ((sB2, sP2), cB) =>
+        cB.fall(map, sB2, sP2)
+      }
+    }
+    finalBeams.map(_.weight).sum
   }
 }
