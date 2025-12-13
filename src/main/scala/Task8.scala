7fcffb3b6b5b43eca51f36e6dd45e0af6f94211c
gaborloi <gaborloi@gmail.com>
2025-12-13 23:20:58 +0100

    Task 8 prep

---
+++ src/main/scala/Task8.scala
@@ -0,0 +1,15 @@
+import Utils.{Cord, fileToCharArray}
+
+import scala.io.BufferedSource
+
+object Task8 {
+  val id = 8
+
+  def solveProblem1(file: BufferedSource, n: Int): Int = {
+    1
+  }
+
+  def solveProblem2(file: BufferedSource): Long = {
+    2L
+  }
+}
