fd19e8f3315de59aa9405da4e9b9a1550ccbdb72
gaborloi <gaborloi@gmail.com>
2025-12-15 00:12:26 +0100

    Task 9 prep

---
+++ src/main/scala/Task9.scala
@@ -0,0 +1,15 @@
+import Utils.{Cord, fileToCharArray}
+
+import scala.io.BufferedSource
+
+object Task9 extends Task {
+  val id = 9
+
+  def solveProblem1(file: BufferedSource): Int = {
+    1
+  }
+
+  def solveProblem2(file: BufferedSource): Long = {
+    2L
+  }
+}
