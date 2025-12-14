711876110a48cd455895fd7c30012b8cea697012
gaborloi <gaborloi@gmail.com>
2025-12-15 00:25:37 +0100

    Task 9.1

--- src/main/scala/Task9.scala
+++ src/main/scala/Task9.scala
@@ -5,10 +5,21 @@ import scala.io.BufferedSource
 object Task9 extends Task {
   val id = 9
 
-  def solveProblem1(file: BufferedSource): Int = {
-    1
+  case class LCord(x: Long, y: Long) {
+    def dist(c2: LCord): Long = math.abs((x-c2.x + 1)*(y-c2.y + 1))
   }
 
+  def solveProblem1(file: BufferedSource): Long = {
+    val cords = file.getLines().map { l =>
+      l.split(',') match
+        case Array(a, b) => LCord(a.toLong, b.toLong)
+    }.toList
+    calcDistances(cords).max
+  }
+
+  def calcDistances(cords: List[LCord]): List[Long] = {
+    (for (i <- cords.indices; j <- Range(i + 1, cords.length)) yield cords(i).dist(cords(j))).toList
+  }
   def solveProblem2(file: BufferedSource): Long = {
     2L
   }
